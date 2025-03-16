351b5516e5c1aaa8d5ac5091629899b6068ad7f4
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-03-16 23:43:06 +0530

    learning

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
+import React from "react";
 
-
-function App() {
- 
+const App = () => {
   return (
-    <>
-     <div className="text-9xl"></div>
-    </>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-6 bg-white rounded-lg shadow-lg">
+        <h1 className="text-2xl font-bold text-gray-800">Hello</h1>
+        <p className="mt-2 text-gray-600">World</p>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
